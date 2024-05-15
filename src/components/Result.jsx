1185051af0621dacff620ca33d21c1f5c0df78e9
unknown <shinchanmusic24@gmail.com>
2024-05-15 15:29:42 +0530

    working on packaging

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -12,55 +12,83 @@ function Result({ saltSuggestions, isLoading }) {
     return (
         <div className='flex items-center flex-col'>
             {saltSuggestions.map((salt, index) => {
-                const [selectForm,setSelectForm] = useState("Tablet")
-                const saltforms = Object.keys(salt.salt_forms_json[selectForm]);
-                const pack = salt.salt_forms_json.Tablet['50mg'] ? Object.keys(salt.salt_forms_json.Tablet['50mg']) : [];
-                const [showMoreStrength, setshowMoreStrength] = useState(false);
-                const [num, setNum] = useState(4);
+                const [selectForm, setSelectForm] = useState("Tablet")
+                const saltForms = salt.salt_forms_json[selectForm];
+                const saltStrengths = Object.keys(saltForms);
+                const [selectedStrength, setSelectedStrength] = useState(saltStrengths[0]);
+                const [showMoreStrength, setShowMoreStrength] = useState(false);
+                const [showMoreForm, setShowMoreForm] = useState(false);
+                const [numStrength, setNumStrength] = useState(4);
+                const [numForm, setNumForm] = useState(4);
 
-                const handleShowMore = () => {
-                    setshowMoreStrength(!showMoreStrength);
-                    showMoreStrength ? setNum(4) : setNum(10);
+                const handleShowMoreStrength = () => {
+                    setShowMoreStrength(!showMoreStrength);
+                    showMoreStrength ? setNumStrength(4) : setNumStrength(10);
+                };
+                const handleShowMoreForm = () => {
+                    setShowMoreForm(!showMoreForm);
+                    showMoreForm ? setNumForm(4) : setNumForm(10);
                 };
 
                 return (
-                    <div className='bg-gradient-to-r from-white via-white to-[#e6f1f1] w-[1007px] min-h-[180px] rounded-lg border flex mt-8 p-2 flex justify-between ' key={index}>
+                    <div className='bg-gradient-to-r from-white via-white to-[#e6f1f1] w-[1007px] min-h-[180px] rounded-lg shadow-lg flex mt-8 p-2 flex justify-between ' key={index}>
                         <div className="resultCardLeft  w-[41%]  flex flex-col justify-between gap-4">
                             <div className='flex justify-start'>
                                 <p className='mr-[37px]'>Forms : </p>
                                 <div className="grid grid-cols-2 gap-2">
-                                    {salt.available_forms.map((form, formIndex) => (
+                                    {(salt.available_forms).slice(0, numForm).map((form, formIndex) => (
                                         <span
-                                         className=' border border-black px-2 py-1 rounded-md text-center cursor-pointer '
-                                         key={formIndex}
-                                         onClick={()=>{setSelectForm(form)}}
-                                         value={form}
-                                         >
+                                            className={`border border-black px-2 py-1 rounded-md text-center cursor-pointer ${form === selectForm ? 'border-green-500' : 'border-gray-300'} ${form === selectForm ? 'font-semibold' : ''}`}
+                                            key={formIndex}
+                                            onClick={() => { setSelectForm(form) }}
+                                            value={form}
+                                        >
                                             {form}
-                                         </span>
+                                        </span>
                                     ))}
+                                    {(salt.available_forms).length > 4 && (
+                                        <button onClick={handleShowMoreForm} className="text-green-500 font-bold ">
+                                            {showMoreForm ? 'Hide' : ' More'}
+                                        </button>
+                                    )}
                                 </div>
                             </div>
                             <div className='flex justify-start'>
                                 <p className='mr-5 '>Strength : </p>
                                 <div className="grid grid-cols-2 gap-2">
-                                    {saltforms.slice(0, num).map((strength, strengthIndex) => (
-                                        <span className=' border border-black px-2 py-1 rounded-md text-center cursor-pointer ' key={strengthIndex}>{strength}</span>
+                                    {saltStrengths.slice(0, numStrength).map((strength, strengthIndex) => (
+                                        <span
+                                            className={`border border-black px-2 py-1 rounded-md text-center cursor-pointer ${strength === selectedStrength ? 'border-green-500' : 'border-gray-300'} ${strength === selectedStrength ? 'font-semibold' : ''}`}
+                                            key={strengthIndex}
+                                            onClick={() => { setSelectedStrength(strength) }}
+                                        >
+                                            {strength}
+                                        </span>
                                     ))}
-                                     {saltforms.length > 4 && (
-                                    <button onClick={handleShowMore} className="text-green-500 font-bold ">
-                                        {showMoreStrength ? 'Hide' : ' More'}
-                                    </button>
-                                )}
+                                    {saltStrengths.length > 4 && (
+                                        <button onClick={handleShowMoreStrength} className="text-green-500 font-bold ">
+                                            {showMoreStrength ? 'Hide' : ' More'}
+                                        </button>
+                                    )}
                                 </div>
-                               
                             </div>
                             <div className='flex justify-start'>
                                 <p className='mr-2 '>Packaging :</p>
                                 <div className="grid grid-cols-2 gap-2">
-                                    {pack.map((key, packIndex) => (
-                                        <span className=' border border-black px-2 py-1 rounded-md text-center cursor-pointer ' key={packIndex}>{key}</span>
-                                    ))}
+                                    {saltForms[selectedStrength] &&
+                                        Object.entries(saltForms[selectedStrength]).map(([packing, products]) => {
+                                           
+                                            
+                                            return (
+                                                <span
+                                                    className='border border-black px-2 py-1 rounded-md text-center cursor-pointer'
+                                                    key={packing}
+                                                >
+                                                    {packing} 
+                                                </span>
+                                            );
+                                        })
+                                    }
                                 </div>
                             </div>
                         </div>
@@ -68,9 +96,30 @@ function Result({ saltSuggestions, isLoading }) {
                             <p className='font-semibold'>{salt.salt}</p>
                             <p className='text-[14px] text-slate-400'>{salt.most_common.Form} | {salt.most_common.Strength} | {salt.most_common.Packing}</p>
                         </div>
-                        <div className="resultCardRight  w-[30%]  flex justify-center items-center">
-                            <p className='text-[28px] font-bold'>From $70</p>
-                        </div>
+                        <div className="resultCardRight w-[30%] flex justify-center items-center">
+    {saltForms[selectedStrength] &&
+        (() => {
+            let lowestPrice = Infinity;
+            let hasAvailableProducts = false;
+            Object.values(saltForms[selectedStrength]).forEach(products => {
+                const availableProducts = Object.values(products).filter(product => Array.isArray(product) && product.length > 0);
+                if (availableProducts.length > 0) {
+                    const price = availableProducts[0][0].selling_price;
+                    if (price < lowestPrice) {
+                        lowestPrice = price;
+                    }
+                    hasAvailableProducts = true;
+                }
+            });
+            if (hasAvailableProducts) {
+                return <p className='text-[28px] font-bold'>From ${lowestPrice}</p>;
+            } else {
+                return <p className='text-[28px] font-bold'>Not available</p>;
+            }
+        })()
+    }
+</div>
+
                     </div>
                 );
             })}
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,9 @@ export default {
     extend: {
       boxShadow: {
         '3xl': '0px 0px 0px 0px rgba(42, 82,130, 1)',
-      }
+        '4xl': '0 35px 60px -15px rgba(61, 255, 85, 1)',
+      },
+     
     },
   },
   plugins: [],
